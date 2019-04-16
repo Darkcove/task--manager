33a484e7f044cc9138d764bda5a25218d9fe1c72
Darkcove <shozdott@gmail.com>
2019-04-16 02:27:49 -0700

    styles edit

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer id="custom-footer">
+    <span class="mr-4">&copy; 2019 David Fernandez</span>
+    <span class="mr-4">Private</span>
+    <span>Terma Of Service</span>
+  </footer>
+</template>
